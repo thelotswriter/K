0cd6f659c12bcb1b2203c88f52ef759c9ab9926b
Brian <thelotswriter@gmail.com>
2017-09-06 11:29:24 -0600

    Displaying probability map demo has begun

--- src/miniMain/DiscreteSearchModel.java
+++ src/miniMain/DiscreteSearchModel.java
@@ -13,6 +13,7 @@ import instructions.InstructionType;
 import pacman.Pacman;
 import processTree.Model;
 import processTree.ThingNode;
+import subModel.Discrete2DSpatialModel;
 import thingNodes.PacmanGame;
 import thingNodes.PacmanGhost;
 
@@ -30,41 +31,44 @@ public class DiscreteSearchModel implements MiniMain
 	{
 		PacmanGame pacmanWorld = new PacmanGame(pMan);
 		PacmanGhost ghost = (PacmanGhost) pacmanWorld.getThing("ghost");
-		Model model = new Model(ghost, pacmanWorld);
-		List<Collection<Instruction>> instructionCollectionList = model.generateActionSequence();
-		int cursorX = pMan.getLocationOnScreen().x + pMan.getGhostX(0) + 16;
-		int cursorY = pMan.getLocationOnScreen().y + pMan.getGhostY(0) + 40;
-		try {
-			Robot robot = new Robot();
-			robot.mouseMove(cursorX, cursorY);
-			for(int i = 0; i < instructionCollectionList.size(); i++)
-			{
-				Collection<Instruction> collection = instructionCollectionList.get(i);
-				for(Instruction instruction : collection)
-				{
-					if(instruction.getType().equals(InstructionType.MOVE))
-					{
-						List<String> moves = instruction.getParameters();
-						cursorX += Integer.parseInt(moves.get(0));
-						cursorY += Integer.parseInt(moves.get(1));
-						System.out.print(cursorX);
-						System.out.print(", ");
-						System.out.println(cursorY);
-						robot.mouseMove(cursorX, cursorY);
-						Thread.sleep(16);
-					} else
-					{
-						System.out.println("Check");
-					}
-				}
-			}
-		} catch (AWTException e) 
-		{
-			e.printStackTrace();
-		} catch (InterruptedException e) 
-		{
-			e.printStackTrace();
-		}
+		Discrete2DSpatialModel discrete2DSpatialModel = new Discrete2DSpatialModel(ghost, pacmanWorld);
+		double[][] probabilityMap = discrete2DSpatialModel.generateProbabilityMap();
+
+//		Model model = new Model(ghost, pacmanWorld);
+//		List<Collection<Instruction>> instructionCollectionList = model.generateActionSequence();
+//		int cursorX = pMan.getLocationOnScreen().x + pMan.getGhostX(0) + 16;
+//		int cursorY = pMan.getLocationOnScreen().y + pMan.getGhostY(0) + 40;
+//		try {
+//			Robot robot = new Robot();
+//			robot.mouseMove(cursorX, cursorY);
+//			for(int i = 0; i < instructionCollectionList.size(); i++)
+//			{
+//				Collection<Instruction> collection = instructionCollectionList.get(i);
+//				for(Instruction instruction : collection)
+//				{
+//					if(instruction.getType().equals(InstructionType.MOVE))
+//					{
+//						List<String> moves = instruction.getParameters();
+//						cursorX += Integer.parseInt(moves.get(0));
+//						cursorY += Integer.parseInt(moves.get(1));
+//						System.out.print(cursorX);
+//						System.out.print(", ");
+//						System.out.println(cursorY);
+//						robot.mouseMove(cursorX, cursorY);
+//						Thread.sleep(16);
+//					} else
+//					{
+//						System.out.println("Check");
+//					}
+//				}
+//			}
+//		} catch (AWTException e)
+//		{
+//			e.printStackTrace();
+//		} catch (InterruptedException e)
+//		{
+//			e.printStackTrace();
+//		}
 	}
 	
 }
